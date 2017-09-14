57f5716ff7009ae6a981b69c7037d8e4cebafba3
michbur <michalburdukiewicz@gmail.com>
2017-09-14 19:18:23 +0200

    fixed pcrfit_paralell

---
+++ R/pcrfit_helper.R
@@ -0,0 +1,160 @@
+prcfit_single <- function(x) {
+  
+  cycles <- 1L:length(x)
+  # Determine highest and lowest amplification curve values
+  fluo_range <- quantile(x, c(0.01, 0.99))
+  
+  # for qpcR
+  dat <- data.frame(cyc = 1, fluo = x)
+  
+  res_bg.max_tmp <- bg.max(cycles, x)
+  length_cycle <- nrow(cycles)
+  res_bg.max <- c(bg.start=res_bg.max_tmp@bg.start/length_cycle,
+                  bg.stop=res_bg.max_tmp@bg.stop/length_cycle,
+                  amp.stop=res_bg.max_tmp@amp.stop/length_cycle)
+  
+  
+  # Determine the head to tail ratio
+  res_head_tail_ratio <- head2tailratio(x)
+  
+  # Determine the slope from the cycles 2 to 11
+  res_lm_fit <- earlyreg(x= cycles, x)
+  
+  # Try to estimate the slope and the intercept of the tail region,
+  # which might be indicative of a hook effect (strong negative slope)
+  res_hookreg <- hookreg(x= cycles, x)
+  
+  # Calculates the area of the amplification curve
+  res_polyarea <- try(polyarea(cycles, x), silent=TRUE)
+  if(class(res_polyarea) == "try-error") {res_polyarea <- NA}
+  
+  # Calculate change points
+  # Agglomerative hierarchical estimation algorithm for multiple change point analysis
+  res_changepoint_e.agglo <- try(length(e.agglo(as.matrix(x))$estimates), silent=TRUE)
+  if(class(res_changepoint_e.agglo) == "try-error") {res_changepoint_e.agglo <- NA}
+  
+  # Bayesian analysis of change points
+  res_bcp_tmp <- bcp(x)            
+  res_bcp_tmp <- res_bcp_tmp$posterior.prob > 0.45
+  res_bcp <- try((which(as.factor(res_bcp_tmp) == TRUE) %>% length))
+  if(class(res_bcp) == "try-error") {res_bcp <- NA}
+  
+  # Median based local robust regression (mblrr)
+  res_mblrr <- mblrr(cycles, x)
+  
+  # Calculate amptester results
+  res_amptester <- try(amptester(x))
+  
+  # Estimate the spread of the approximate local minima and maxima of the curve data
+  dat_smoothed <- smoother(cycles, x)
+  
+  res_diffQ <- diffQ(cbind(cycles, dat_smoothed), verbose = TRUE)$xy
+  res_mcaPeaks <- mcaPeaks(res_diffQ[, 1], res_diffQ[, 2])
+  mcaPeaks_minima_maxima_ratio <- diff(range(res_mcaPeaks$p.max[, 2])) / diff(range(res_mcaPeaks$p.min[, 2]))
+  
+  # Estimate the slope between the minimum and the maximum of the second derivative
+  res_diffQ2 <- diffQ2(cbind(cycles, dat_smoothed), verbose=FALSE, fct=min)
+  range_Cq <- diff(res_diffQ2[[3]])
+  if(res_diffQ2[[3]][1] < res_diffQ2[1] && res_diffQ2[1] < res_diffQ2[[3]][2]) {range_Cq} else {range_Cq <- 0}
+  if(res_diffQ2[[3]][1] < res_diffQ2[1] && res_diffQ2[1] < res_diffQ2[[3]][2] && range_Cq > 1 && range_Cq < 9) {
+    res_diffQ2_slope <- coefficients(lm(unlist(c(res_diffQ2[[4]])) ~ unlist(c(res_diffQ2[[3]]))))[2]
+  } else {res_diffQ2_slope <- 0}
+  
+  # Perform an autocorrelation analysis           
+  res_autocorrelation <- autocorrelation_test(y=x)
+  
+  # Fit sigmoidal models to curve data
+  res_fit <- try(mselect(pcrfit(dat, cyc=1, fluo=2), 
+                         verbose=FALSE, do.all=TRUE), silent=TRUE)
+  
+  # Determine the model suggested by the mselect function based on the AICc
+  res_fit_model <- try(names(which(res_fit[["retMat"]][, "AICc"] == min(res_fit[["retMat"]][, "AICc"]))), silent=TRUE)
+  if(class(res_fit_model) == "try-error") {res_fit_model <- NA}
+  
+  if(class(res_fit)[1] != "try-error") {
+    # TakeOff Point
+    # Calculates the first significant cycle of the exponential region 
+    # (takeoff point) using externally studentized residuals as described 
+    # in Tichopad et al. (2003).
+    res_takeoff <- try(takeoff(res_fit), silent=TRUE)
+    if(class(res_takeoff) == "try-error") {res_takeoff <- list(NA, NA)}
+    
+    # LRE qPCR efficiency
+    # Calculation of qPCR efficiency by the 'linear regression of 
+    # efficiency' method
+    
+    res_LRE <- try(LRE(res_fit, plot=FALSE, verbose=FALSE)$eff, silent=TRUE)
+    if(class(res_LRE) == "try-error") {res_LRE <- NA}
+    
+    # sliwin qPCR efficiency
+    # Calculation of the qPCR efficiency by the 'window-of-linearity' method
+    res_sliwin <- try(sliwin(res_fit, plot=FALSE, verbose=FALSE)$eff, 
+                      silent=TRUE)
+    if(class(res_sliwin) == "try-error") {res_sliwin <- NA}
+    
+    # Cq of the amplification curve
+    # Determine the Cq and other parameters
+    res_efficiency_tmp <- try(
+      efficiency(res_fit, plot=FALSE)[c("eff",
+                                        "cpD1", "cpD2",
+                                        "fluo",
+                                        "init1", "init2")], 
+      silent=TRUE)
+    if(class(res_efficiency_tmp) != "try-error") {                        
+      res_cpDdiff <- try(res_efficiency_tmp[["cpD1"]] - res_efficiency_tmp[["cpD2"]])
+    } else {
+      res_efficiency_tmp <- list(NA, NA, NA, NA, NA, NA)
+      res_cpDdiff <- NA
+    }
+  } else {
+    res_efficiency_tmp <- list(NA, NA, NA, NA, NA, NA)
+    res_takeoff <- list(NA, NA)
+    res_LRE <- NA
+    res_sliwin <- NA
+    res_cpDdiff <- NA
+  }
+  
+  res_efficiency <- list(
+    eff=res_efficiency_tmp[["eff"]],
+    cpD1=res_efficiency_tmp[["cpD1"]],
+    cpD2=res_efficiency_tmp[["cpD2"]],
+    fluo=res_efficiency_tmp[["fluo"]],
+    init1=res_efficiency_tmp[["init1"]],
+    init2=res_efficiency_tmp[["init2"]],
+    top=res_takeoff[[1]], 
+    f.top=res_takeoff[[2]],
+    resLRE=res_LRE[1],
+    ressliwin=res_sliwin[[1]],
+    cpDdiff=res_cpDdiff,
+    slope_background=res_lm_fit[["slope"]],
+    intercept_background=res_lm_fit[["intercept"]],
+    polyarea=res_polyarea,
+    changepoint.e.agglo=res_changepoint_e.agglo,
+    changepoint.bcp=res_bcp,
+    qPCRmodel=res_fit_model[[1]],
+    amptester_shap.noisy=res_amptester@decisions["shap.noisy"][[1]],
+    amptester_lrt.test=res_amptester@decisions["lrt.test"][[1]],
+    amptester_rgt.dec=res_amptester@decisions["rgt.dec"][[1]],
+    amptester_tht.dec=res_amptester@decisions["tht.dec"][[1]],
+    amptester_slt.dec=res_amptester@decisions["slt.dec"][[1]],
+    amptester_polygon=res_amptester@"polygon",
+    amptester_slope.ratio=res_amptester@"slope.ratio",
+    minRFU=fluo_range[[1]], 
+    maxRFU=fluo_range[[2]],
+    bg.start_normalized=res_bg.max[1],
+    bg.stop_normalized=res_bg.max[2],
+    amp.stop_normalized=res_bg.max[3],
+    head_to_tail_ratio=res_head_tail_ratio,
+    autocorellation=res_autocorrelation,
+    mblrr_intercept_less=res_mblrr[1],
+    mblrr_slope_less=res_mblrr[2],
+    mblrr_cor_less=res_mblrr[3],
+    mblrr_intercept_more=res_mblrr[4],
+    mblrr_slope_more=res_mblrr[5],
+    mblrr_cor_more=res_mblrr[6],
+    hookreg_hook=res_hookreg[["hook"]],
+    mcaPeaks_minima_maxima_ratio=mcaPeaks_minima_maxima_ratio,
+    diffQ2_slope=res_diffQ2_slope,
+    diffQ2_Cq_range=range_Cq
+  )
+}
--- R/pcrfit_parallel.R
+++ R/pcrfit_parallel.R
@@ -28,7 +28,7 @@
 #'  
 #' @export pcrfit_parallel
 
-pcrfit_parallel <- function(data, n_cores = 2, detection_chemistry=NA, device=NA) {
+pcrfit_parallel <- function(data, n_cores = 1, detection_chemistry=NA, device=NA) {
     # Determine the number of available cores and registrate them    
     if(n_cores == "all")
       n_cores <- detectCores() 
@@ -44,233 +44,19 @@ pcrfit_parallel <- function(data, n_cores = 2, detection_chemistry=NA, device=NA
         data_RFU[, i] / quantile(data_RFU[, i], 0.999)
     })
     colnames(data_RFU) <- data_RFU_colnames
-    
-    # Cut the data.frame into smaller blocks for parallel analysis
-    if(ncol(data_RFU) <= 3) {
-        registerDoParallel(1)
-        cuts <- rep(1, ncol(data_RFU))
-    } else {
-        cuts <- cut(1L:ncol(data_RFU), breaks = n_cores, label=FALSE)
-    }
-    
-    # Define names of feature labels used for the ML analysis
-    features <- c("eff",
-                  "cpD1",
-                  "cpD2",
-                  "fluo",
-                  "init1",
-                  "init2",
-                  "top", 
-                  "f.top",
-                  "resLRE",
-                  "ressliwin",
-                  "cpDdiff",
-                  "slope_background",
-                  "intercept_background",
-                  "polyarea",
-                  "changepoint.e.agglo",
-                  "changepoint.bcp",
-                  "qPCRmodel",
-                  "amptester_shap.noisy",
-                  "amptester_lrt.test",
-                  "amptester_rgt.dec",
-                  "amptester_tht.dec",
-                  "amptester_slt.dec",
-                  "amptester_polygon",
-                  "amptester_slope.ratio",
-                  "minRFU", 
-                  "maxRFU",
-                  "head_to_tail_ratio",
-                  "bg.start_normalized",
-                  "bg.stop_normalized",
-                  "amp.stop_normalized",
-                  "autocorellation",
-                  "detection_chemistry",
-                  "device",
-                  "mblrr_intercept_less",
-                  "mblrr_slope_less",
-                  "mblrr_cor_less",
-                  "mblrr_intercept_more",
-                  "mblrr_slope_more",
-                  "mblrr_cor_more",
-                  "hookreg_hook",
-                  "mcaPeaks_minima_maxima_ratio",
-                  "diffQ2_slope",
-                  "diffQ2_Cq_range"
-                  )
-    
-    # Do the parallel analysis
-    block <- unique(cuts)
-    res_tmp <- foreach(block, 
-                       .packages=c("bcp", "changepoint", "chipPCR", "ecp", "MBmca",
-                                   "PCRedux", "pracma", "qpcR", "robustbase", 
-                                   "zoo"), .combine=cbind, 
-                                   .export=c("simple_fn")) %dopar% {
-        
-        # Combine the cycle values and a specific data block that was previously 
-        # cutted from the input data
-        # Select the appropriate data for a further analysis
-        dat <- data.frame(cycles, data_RFU[, which(cuts == block)])
-        column_names <- colnames(dat)
-        
-        res_block <- sapply(2L:ncol(dat), simplify="array", function(bc) {
 
-            # Determine highest and lowest amplification curve values
-            fluo_range <- quantile(dat[, bc], c(0.01, 0.99))
-            
-            res_bg.max_tmp <- bg.max(unlist(cycles), dat[, bc])
-            length_cycle <- nrow(cycles)
-            res_bg.max <- c(bg.start=res_bg.max_tmp@bg.start/length_cycle,
-                                bg.stop=res_bg.max_tmp@bg.stop/length_cycle,
-                                amp.stop=res_bg.max_tmp@amp.stop/length_cycle)
- 
-            
-            # Determine the head to tail ratio
-            res_head_tail_ratio <- head2tailratio(dat[, bc])
-            
-            # Determine the slope from the cycles 2 to 11
-            res_lm_fit <- earlyreg(x= dat[, 1], dat[, bc])
-            
-            # Try to estimate the slope and the intercept of the tail region,
-            # which might be indicative of a hook effect (strong negative slope)
-            res_hookreg <- hookreg(x= dat[, 1], dat[, bc])
-            
-            # Calculates the area of the amplification curve
-            res_polyarea <- try(polyarea(dat[, 1], dat[, bc]), silent=TRUE)
-            if(class(res_polyarea) == "try-error") {res_polyarea <- NA}
-            
-            # Calculate change points
-            # Agglomerative hierarchical estimation algorithm for multiple change point analysis
-            res_changepoint_e.agglo <- try(length(e.agglo(as.matrix(dat[, bc]))$estimates), silent=TRUE)
-            if(class(res_changepoint_e.agglo) == "try-error") {res_changepoint_e.agglo <- NA}
-                       
-            # Bayesian analysis of change points
-            res_bcp_tmp <- bcp(dat[, bc])            
-            res_bcp_tmp <- res_bcp_tmp$posterior.prob > 0.45
-            res_bcp <- try((which(as.factor(res_bcp_tmp) == TRUE) %>% length))
-            if(class(res_bcp) == "try-error") {res_bcp <- NA}
-            
-            # Median based local robust regression (mblrr)
-            res_mblrr <- mblrr(dat[, 1], dat[, bc])
-
-            # Calculate amptester results
-            res_amptester <- try(amptester(dat[, bc]))
-            
-            # Estimate the spread of the approximate local minima and maxima of the curve data
-            dat_smoothed <- smoother(dat[, 1], dat[, bc])
-            
-            res_diffQ <- diffQ(cbind(dat[, 1], dat_smoothed), verbose = TRUE)$xy
-            res_mcaPeaks <- mcaPeaks(res_diffQ[, 1], res_diffQ[, 2])
-            mcaPeaks_minima_maxima_ratio <- diff(range(res_mcaPeaks$p.max[, 2])) / diff(range(res_mcaPeaks$p.min[, 2]))
-            
-            # Estimate the slope between the minimum and the maximum of the second derivative
-            res_diffQ2 <- diffQ2(cbind(dat[, 1], dat_smoothed), verbose=FALSE, fct=min)
-            range_Cq <- diff(res_diffQ2[[3]])
-            if(res_diffQ2[[3]][1] < res_diffQ2[1] && res_diffQ2[1] < res_diffQ2[[3]][2]) {range_Cq} else {range_Cq <- 0}
-            if(res_diffQ2[[3]][1] < res_diffQ2[1] && res_diffQ2[1] < res_diffQ2[[3]][2] && range_Cq > 1 && range_Cq < 9) {
-                res_diffQ2_slope <- coefficients(lm(unlist(c(res_diffQ2[[4]])) ~ unlist(c(res_diffQ2[[3]]))))[2]
-            } else {res_diffQ2_slope <- 0}
-            
-            # Perform an autocorrelation analysis           
-            res_autocorrelation <- autocorrelation_test(y=dat[, bc])
-            
-            # Fit sigmoidal models to curve data
-            res_fit <- try(mselect(pcrfit(dat, cyc=1, fluo=bc), 
-                                   verbose=FALSE, do.all=TRUE), silent=TRUE)
-
-            # Determine the model suggested by the mselect function based on the AICc
-            res_fit_model <- try(names(which(res_fit[["retMat"]][, "AICc"] == min(res_fit[["retMat"]][, "AICc"]))), silent=TRUE)
-            if(class(res_fit_model) == "try-error") {res_fit_model <- NA}
-            
-            if(class(res_fit)[1] != "try-error") {
-                # TakeOff Point
-                # Calculates the first significant cycle of the exponential region 
-                # (takeoff point) using externally studentized residuals as described 
-                # in Tichopad et al. (2003).
-                res_takeoff <- try(takeoff(res_fit), silent=TRUE)
-                if(class(res_takeoff) == "try-error") {res_takeoff <- list(NA, NA)}
-                
-                # LRE qPCR efficiency
-                # Calculation of qPCR efficiency by the 'linear regression of 
-                # efficiency' method
-                
-                res_LRE <- try(LRE(res_fit, plot=FALSE, verbose=FALSE)$eff, silent=TRUE)
-                if(class(res_LRE) == "try-error") {res_LRE <- NA}
-                
-                # sliwin qPCR efficiency
-                # Calculation of the qPCR efficiency by the 'window-of-linearity' method
-                res_sliwin <- try(sliwin(res_fit, plot=FALSE, verbose=FALSE)$eff, 
-                                  silent=TRUE)
-                if(class(res_sliwin) == "try-error") {res_sliwin <- NA}
-                
-                # Cq of the amplification curve
-                # Determine the Cq and other parameters
-                res_efficiency_tmp <- try(
-                    efficiency(res_fit, plot=FALSE)[c("eff",
-                                                      "cpD1", "cpD2",
-                                                      "fluo",
-                                                      "init1", "init2")], 
-                                          silent=TRUE)
-                if(class(res_efficiency_tmp) != "try-error") {                        
-                    res_cpDdiff <- try(res_efficiency_tmp[["cpD1"]] - res_efficiency_tmp[["cpD2"]])
-                } else {
-                    res_efficiency_tmp <- list(NA, NA, NA, NA, NA, NA)
-                    res_cpDdiff <- NA
-                }
-            } else {
-                res_efficiency_tmp <- list(NA, NA, NA, NA, NA, NA)
-                res_takeoff <- list(NA, NA)
-                res_LRE <- NA
-                res_sliwin <- NA
-                res_cpDdiff <- NA
-            }
-  
-            res_efficiency <- list(
-                    eff=res_efficiency_tmp[["eff"]],
-                    cpD1=res_efficiency_tmp[["cpD1"]],
-                    cpD2=res_efficiency_tmp[["cpD2"]],
-                    fluo=res_efficiency_tmp[["fluo"]],
-                    init1=res_efficiency_tmp[["init1"]],
-                    init2=res_efficiency_tmp[["init2"]],
-                    top=res_takeoff[[1]], 
-                    f.top=res_takeoff[[2]],
-                    resLRE=res_LRE[1],
-                    ressliwin=res_sliwin[[1]],
-                    cpDdiff=res_cpDdiff,
-                    slope_background=res_lm_fit[["slope"]],
-                    intercept_background=res_lm_fit[["intercept"]],
-                    polyarea=res_polyarea,
-                    changepoint.e.agglo=res_changepoint_e.agglo,
-                    changepoint.bcp=res_bcp,
-                    qPCRmodel=res_fit_model[[1]],
-                    amptester_shap.noisy=res_amptester@decisions["shap.noisy"][[1]],
-                    amptester_lrt.test=res_amptester@decisions["lrt.test"][[1]],
-                    amptester_rgt.dec=res_amptester@decisions["rgt.dec"][[1]],
-                    amptester_tht.dec=res_amptester@decisions["tht.dec"][[1]],
-                    amptester_slt.dec=res_amptester@decisions["slt.dec"][[1]],
-                    amptester_polygon=res_amptester@"polygon",
-                    amptester_slope.ratio=res_amptester@"slope.ratio",
-                    minRFU=fluo_range[[1]], 
-                    maxRFU=fluo_range[[2]],
-                    bg.start_normalized=res_bg.max[1],
-                    bg.stop_normalized=res_bg.max[2],
-                    amp.stop_normalized=res_bg.max[3],
-                    head_to_tail_ratio=res_head_tail_ratio,
-                    autocorellation=res_autocorrelation,
-                    detection_chemistry=detection_chemistry,
-                    device=device,
-                    mblrr_intercept_less=res_mblrr[1],
-                    mblrr_slope_less=res_mblrr[2],
-                    mblrr_cor_less=res_mblrr[3],
-                    mblrr_intercept_more=res_mblrr[4],
-                    mblrr_slope_more=res_mblrr[5],
-                    mblrr_cor_more=res_mblrr[6],
-                    hookreg_hook=res_hookreg[["hook"]],
-                    mcaPeaks_minima_maxima_ratio=mcaPeaks_minima_maxima_ratio,
-                    diffQ2_slope=res_diffQ2_slope,
-                    diffQ2_Cq_range=range_Cq
-                )
-        })
-    }
-#     dimnames(res_tmp)[[2]] <- colnames(data[-1])
+    # just to shut RCHeck for NSE we define ith_cycle
+    ith_cycle <- 1
+    run_res <- foreach(ith_cycle = 1L:ncol(data_RFU), 
+            .packages=c("bcp", "changepoint", "chipPCR", "ecp", "MBmca",
+                        "PCRedux", "pracma", "qpcR", "robustbase", 
+                        "zoo"),
+            .combine = rbind) %dopar% {
+                          prcfit_single(data_RFU[, ith_cycle])
+                        }
+    
+    res <- cbind(runs = colnames(data_RFU), do.call(rbind, run_res))
+    
+    res
+    
 }
