f076b74719902fa632c9bf7ad53bdb799f1c0357
devSJR <stefan.roediger@b-tu.de>
2017-09-19 09:26:28 +0200

    fix

--- R/mblrr.R
+++ R/mblrr.R
@@ -34,7 +34,7 @@ function(x, y, sig.level=0.01) {
             res_less_than_median <- y < median(y)
             res_more_than_median <- y > median(y)
             
-            res_mblrr <- c(NA, NA, NA, NA, NA, NA, NA)
+            res_mblrr <- c(NA, NA, NA, NA, NA, NA)
             
             if(class(res_less_than_median) == "logical" && class(res_less_than_median) == "logical") {
                 res_less_than_median_lm <- try(lmrob(y[res_less_than_median] ~ x[res_less_than_median]), silent=TRUE)
