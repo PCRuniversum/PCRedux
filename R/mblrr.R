13c13ac7cae38cb87f522a5afd79c35ddb98d696
devSJR <stefan.roediger@b-tu.de>
2017-09-25 00:26:39 +0200

    final fixes?

--- R/mblrr.R
+++ R/mblrr.R
@@ -32,42 +32,62 @@
 #' @export mblrr
 mblrr <-
 function(x, y, sig.level=0.01, normalize=FALSE) {
-            
-            if(normalize) y <- y/quantile(y, 0.999)
-            
-            res_q25 <- y < quantile(y, 0.25)
-            res_q75 <- y > quantile(y, 0.75)
 
             res_mblrr <- c(NA, NA, NA, NA, NA, NA)
+
+            if(normalize) y <- y/quantile(y, 0.999, na.rm=TRUE)
+
+            res_q25 <- y < quantile(y, 0.25, na.rm=TRUE)
+            res_q75 <- y > quantile(y, 0.75, na.rm=TRUE)
+
+            res_q25_lm <- try(suppressWarnings(lmrob(y[res_q25] ~ x[res_q25])), silent=TRUE)
+            res_q75_lm <- try(suppressWarnings(lmrob(y[res_q75] ~ x[res_q75])), silent=TRUE)
             
-            if(class(res_q25) == "logical" && class(res_q25) == "logical") {
-                res_q25_lm <- try(suppressWarnings(lmrob(y[res_q25] ~ x[res_q25])), silent=TRUE)
-                res_q75_lm <- try(suppressWarnings(lmrob(y[res_q75] ~ x[res_q75])), silent=TRUE)
-                
-                if(class(res_q25_lm) != "try-error" & class(res_q75_lm) != "try-error"){
-                    res_q25_cor.test <- try(cor.test(x[res_q25], y[res_q25]), silent=TRUE)
-                    res_q75_cor.test <- try(cor.test(x[res_q75], y[res_q75]), silent=TRUE)
-                    
-                    if(class(res_q25_cor.test) != "try-error" & class(res_q75_cor.test) != "try-error"){
-                        ifelse(res_q25_cor.test$p.value <= sig.level, 
-                                        res_q25_cor.test_estimate <- res_q25_cor.test$estimate, 
-                                        res_q25_cor.test_estimate <- NA
-                              )
-                        ifelse(res_q75_cor.test$p.value <= sig.level, 
-                                        res_q75_cor.test_estimate <- res_q75_cor.test$estimate, 
-                                        res_q75_cor.test_estimate <- NA
-                              )
-                    }
-                res_mblrr <- c(res_q25_lm$coefficients[1],
-                               res_q25_lm$coefficients[2],
-                               res_q25_cor.test_estimate,
-                               res_q75_lm$coefficients[1],
-                               res_q75_lm$coefficients[2],
-                               res_q75_cor.test_estimate
-                               )
-                                
-                }
-            } 
+            res_q25_cor.test <- try(cor.test(x[res_q25], y[res_q25]), silent=TRUE)
+            res_q75_cor.test <- try(cor.test(x[res_q75], y[res_q75]), silent=TRUE)
+
+            if(class(res_q25_lm) == "try-error" || is.na(res_q25_lm[[1]])[1] == TRUE) {
+                                    mblrr_intercept_less <- NA
+                                    mblrr_slope_less <- NA
+                                    } else {
+                                            mblrr_intercept_less <- res_q25_lm$coefficients[[1]]
+                                            mblrr_slope_less <- res_q25_lm$coefficients[[2]]
+                                    }
+            
+            if(class(res_q75_lm) == "try-error" || is.na(res_q75_lm[[1]])[1] == TRUE) {
+                        mblrr_intercept_more <- NA
+                        mblrr_slope_more <- NA
+                        } else {
+                                mblrr_intercept_more <- res_q75_lm$coefficients[[1]]
+                                mblrr_slope_more <- res_q75_lm$coefficients[[2]]
+                        }
+            
+           
+
+            if(class(res_q25_cor.test) == "try-error" || is.na(res_q25_cor.test$p.value)) {
+                                            res_q25_cor.test_estimate <- NA
+                                    } else {
+                                            ifelse(res_q25_cor.test$p.value < sig.level,  
+                                                   res_q25_cor.test_estimate <- res_q25_cor.test$estimate, 
+                                                   res_q25_cor.test_estimate <- NA)
+                                            }
+
+            if(class(res_q75_cor.test) == "try-error" || is.na(res_q75_cor.test$p.value)) {
+                                            res_q75_cor.test_estimate <- NA
+                                    } else {
+                                            ifelse(res_q75_cor.test$p.value < sig.level,  
+                                                   res_q75_cor.test_estimate <- res_q75_cor.test$estimate, 
+                                                   res_q75_cor.test_estimate <- NA)
+                                            }
+                                            
+
+            res_mblrr <- c(mblrr_intercept_less,
+                           mblrr_slope_less,
+                           res_q25_cor.test_estimate,
+                           mblrr_intercept_more,
+                           mblrr_slope_more,
+                           res_q75_cor.test_estimate
+                        )
             
             names(res_mblrr) <- c("mblrr_intercept_less",
                                   "mblrr_slope_less",
@@ -75,5 +95,5 @@ function(x, y, sig.level=0.01, normalize=FALSE) {
                                   "mblrr_intercept_more",
                                   "mblrr_slope_more",
                                   "mblrr_cor_more")
-            res_mblrr                          
+            res_mblrr
 }
