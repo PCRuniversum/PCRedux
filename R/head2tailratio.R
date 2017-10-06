d07a093232a6e2fe673434231804194523708051
devSJR <stefan.roediger@b-tu.de>
2017-10-06 17:56:12 +0200

    serveral refinements to head2tailratio (plotting) and better documentation

--- R/head2tailratio.R
+++ R/head2tailratio.R
@@ -10,6 +10,8 @@
 #' @param normalize is a logical parameter, which indicates if the amplification curve.
 #' @param slope_normalizer is a logical parameter, which indicates if the 
 #' head2tailratio should be normalized to the slope of the ROI.
+#' @param verbose  is a logical parameter, which indicates if all the values, 
+#' parameters and coefficients of the ananlysis should be shown.
 #' @author Stefan Roediger, Michal Burdukiewcz
 #' @keywords ratio head tail
 #' @examples
@@ -28,7 +30,7 @@
 #'         main="competimer dataset", type="l", lty=1, col=res_head2tailratio_cluster, lwd=2)
 #'
 #' @export head2tailratio
-head2tailratio <- function(y, normalize=FALSE, slope_normalizer=FALSE) {
+head2tailratio <- function(y, normalize=FALSE, slope_normalizer=FALSE, verbose=FALSE) {
 
     # Remove missing values
     y <-na.omit(y)
@@ -41,6 +43,8 @@ head2tailratio <- function(y, normalize=FALSE, slope_normalizer=FALSE) {
     y_head <- head(y)
     y_tail <- tail(y)
     
+    x_reg <- c(which(y %in% y_head), which(y %in% y_tail))
+    y_reg <- c(y_head,y_tail)
     
     # Calculate the ratio of the head and the tail
     res_head_tail_ratio <- try(median(y_head) / median(y_tail))
@@ -48,9 +52,6 @@ head2tailratio <- function(y, normalize=FALSE, slope_normalizer=FALSE) {
     # Normalize the the head/tail ratio to the slope of the data set,
     # provided that the slope is significant.
     if(slope_normalizer) {
-                x_reg <- c(which(y %in% y_head), which(y %in% y_tail))
-                y_reg <- c(y_head,y_tail)
-                
                 head_tail_fit <- try(lm(y_reg ~ x_reg))
                 res_lm_fit_summary <- try(summary(head_tail_fit))$coefficients[2, 4]
                 
@@ -63,8 +64,14 @@ head2tailratio <- function(y, normalize=FALSE, slope_normalizer=FALSE) {
                 }
     }
     
+    res_head_tail_ratio_verbose <- list(x_roi=x_reg, y_roi=y_reg, fit_lm=head_tail_fit, head_tail_ratio=res_head_tail_ratio)
+    
     if(class(res_head_tail_ratio) != "numeric") {
         res_head_tail_ratio <- NA
         }
-    res_head_tail_ratio
+    if(verbose){
+         res_head_tail_ratio_verbose
+    }else{
+         res_head_tail_ratio
+    }
 }
