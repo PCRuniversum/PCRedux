c02ab5d03000fd07ee29a4239b43b42b049339d3
devSJR <stefan.roediger@b-tu.de>
2017-10-26 12:01:09 +0200

    another test added

--- tests/testthat/test_hookregNL.R
+++ tests/testthat/test_hookregNL.R
@@ -6,10 +6,13 @@ test_that("hookregNL gives the correct dimensions and properties", {
 
     library(qpcR)
     res_hookregNL_positve <- hookregNL(x=boggy[, 1], y=boggy[, 2])
+    res_hookregNL_positve_complex <- hookregNL(x=boggy[, 1], y=boggy[, 2], simple=FALSE)
     res_hookregNL_negative <- hookregNL(x=boggy[, 1], y=boggy[, 6])
 
     expect_that(res_hookregNL_positve, is_a("data.frame"))
+    expect_that(res_hookregNL_positve_complex, is_a("list"))
     expect_that(res_hookregNL_negative, is_a("data.frame"))
     expect_that(res_hookregNL_positve[["hook"]] == 1, is_true())
+    expect_that(res_hookregNL_positve_complex[["hook"]] == 1, is_true())
     expect_that(res_hookregNL_negative[["hook"]] == 1, is_false())
 })
