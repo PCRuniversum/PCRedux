73b71530c6f89326fb78c9c844d8b2d6d22be6bf
devSJR <stefan.roediger@b-tu.de>
2017-11-05 16:16:57 +0100

    tests improved

--- tests/testthat/test_hookregNL.R
+++ tests/testthat/test_hookregNL.R
@@ -10,9 +10,12 @@ test_that("hookregNL gives the correct dimensions and properties", {
     res_hookregNL_negative <- hookregNL(x=boggy[, 1], y=boggy[, 6])
 
     expect_that(res_hookregNL_positve, is_a("data.frame"))
+    expect_that(round(res_hookregNL_positve[["slope"]], 8) == -0.01596799, is_true())
     expect_that(res_hookregNL_positve_complex, is_a("list"))
     expect_that(res_hookregNL_negative, is_a("data.frame"))
     expect_that(res_hookregNL_positve[["hook"]] == 1, is_true())
     expect_that(res_hookregNL_positve_complex[["hook"]] == 1, is_true())
+    expect_that(res_hookregNL_positve_complex[["fit"]][["weights"]][1] == 1, is_true())
+    expect_that(round(res_hookregNL_positve_complex[["slope"]], 8) == -0.01596799, is_true())
     expect_that(res_hookregNL_negative[["hook"]] == 1, is_false())
 })
--- tests/testthat/test_mblrr.R
+++ tests/testthat/test_mblrr.R
@@ -13,4 +13,7 @@ test_that("mblrr gives the correct dimensions and properties", {
     expect_that(length(res) == 6, is_true())
     expect_that(is.na(res[["mblrr_cor_more"]]) == TRUE, is_true())
     expect_that(res[["mblrr_cor_less"]] >= 0.80067 && res[["mblrr_cor_less"]] <= 0.800679, is_true())
+    expect_that(res[["mblrr_cor_less"]] >= 0.80067 && res[["mblrr_cor_less"]] <= 0.800679, is_true())
+    expect_that(round(res[["mblrr_intercept_more"]], 6) == 1.037105, is_true())
+    expect_that(round(res[["mblrr_slope_more"]], 6) == -0.001629, is_true())
 })
